1c5aa26608edfdea490cd9f5b9fd5499c1d634ba
anishacharya0099-hub <anishacharya0099@gmail.com>
2026-03-28 22:37:02 +0200

    Update 123456.js

--- 123456.js
+++ 123456.js
@@ -21,6 +21,10 @@ function startScanner() {
         function (text) {
             const place = JSON.parse(text);
             showMarkerAt(place.top, place.left);
+
+            // NEW: Show inventory details
+            showItemInfo(item);
+            
             toggleScanner();
         }
     ).catch(function (err) {
@@ -35,4 +39,10 @@ function stopScanner() {
 function showMarkerAt(top, left) {
     marker.style.top = top;
     marker.style.left = left;
+}
+
+function showItemInfo(item) {
+    name.innerText = "Name: " + item.name;
+    stock.innerText = "In store: " + (item.in_store ? "Yes" : "No");
+    price.innerText = "Price: €" + item.price;
 }
